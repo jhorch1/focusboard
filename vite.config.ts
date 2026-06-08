b3a3bbf3904d5d2b097f354f21b2ff4c2fcdd0ad
maria1625 <166460177+maria1625@users.noreply.github.com>
2026-06-08 18:28:41 -0500

    Prueba unitaria: lógica del timer Pomodoro closes.#8

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    include: ['test/**/*.test.ts', 'test/**/*.test.tsx'],
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
